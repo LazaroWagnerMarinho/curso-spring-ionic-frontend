d4807c48dd732cffadf3a23286a526e8ca077bd2
Lazaro Marinho <LazaroWagnerMarinho@hotmail.com>
2020-06-27 23:53:18 -0300

    Organizando pastas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('../pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'categorias',
-    loadChildren: () => import('./categorias/categorias.module').then( m => m.CategoriasPageModule)
+    loadChildren: () => import('../pages/categorias/categorias.module').then( m => m.CategoriasPageModule)
   },
 
 ];
